949d2757deb47018f0fe22fb5bdfc3e79c93a6cc
Zi Tao (Chloe) <chloetaozi@gmail.com>
2023-09-11 23:18:00 +1000

    finish serializers for contact

---
+++ crm/contact/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from contact.models import Contact
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Contact
+        fields=('contact_id', 'first_name', 'last_name', 'gender', 'date_of_birth', 'address', 
+                'phone_number', 'contact_email')
